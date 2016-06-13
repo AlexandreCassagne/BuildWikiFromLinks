a66b7fd370f639e920b052d5a7210328ba3b3a65
Alexandre Cassagne <alexandre@travl.co>
2016-06-13 14:12:54 +0200

    Initial Commit

---
+++ BuildWikiFromLinks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BuildWikiFromLinks
+//
+//  Created by Alexandre Cassagne on 13/06/2016.
+//  Copyright © 2016 Alexandre Cassagne. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
